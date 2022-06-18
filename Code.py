9ffafe15d716aa48f7ab9b8c561dbd5fbb7502dd
Madhavi-Raj <76896869+Madhavi-Raj@users.noreply.github.com>
2022-06-18 20:35:27 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,9 +2,10 @@ from pylatex import Document, Section, Subsection,MiniPage
 from pylatex.utils import italic, bold
 from pylatex import Document, Section, Subsection, Tabular,basic,position,utils
 from pylatex import base_classes,MiniPage,Package,NoEscape,NewPage
-doc.packages.append(Package('adjustbox'))
+import pylatex as pl
+
 geometry_options = {"tmargin": "10mm", "lmargin": "10mm","margin": "10mm"}
-doc = Document(geometry_options=geometry_options,font_size =" ")
+doc = Document(geometry_options=geometry_options)
 
 
 import pandas as pd
@@ -28,43 +29,43 @@ d[chapter_name]=topics
 for i in d:
     doc.append(NoEscape(r'\begin{center}'))
     doc.append(NoEscape(r'\textbf{Learn Basics - LPS}'+' Course Release Schedule [PT2]'))
-    doc.append("\n")
+    doc.append(NoEscape(r'\end{center}'))
     doc.append(NoEscape(r'\par\noindent\rule{\textwidth}{0.4pt}'))
-    doc.append("\n")
     h=r'\textbf{'+i+'}'
+    doc.append(NoEscape(r'\begin{center}'))
     doc.append(NoEscape(h))
-    doc.append("\n")
+    doc.append(NoEscape(r'\end{center}'))
     subject="Science - Biology"
     class1=str(7)
     chaptertag=d[i][0]
-    doc.append(NoEscape(r'\vspace{2mm}'))
-    doc.append("\n")
-    with doc.create(Tabular('p{8.5cm}|p{8.5cm}')) as table:
-            table.add_hline()
-            table.add_row("subject: "+subject,"Class: "+class1)
-            table.add_empty_row()
-            table.add_hline()
-            table.add_row("Chapter Tag : "+chaptertag,"Teacher Name : ")
-            table.add_empty_row()
-    doc.append("\n")
-    doc.append(NoEscape(r'\end{center}'))
-    doc.append('Start Date:________________________') 
     doc.append(NoEscape(r'\vspace{3mm}'))
+    doc.append(NoEscape(r'\renewcommand*{\arraystretch}{2}'))
+    with doc.create(Tabular('p{8.5cm}|p{8.5cm}')) as table:
+        table.add_hline()
+        table.add_row("Subject: "+subject,"Class: "+class1)
+        table.add_hline()
+        table.add_row("Chapter Tag : "+chaptertag,"Teacher Name : ")
+    doc.append(NoEscape(r'\vspace{5mm}'))
+    doc.append("\n") 
+    doc.append('Start Date:________________________') 
+    doc.append(NoEscape(r'\vspace{5mm}'))
     doc.append("\n")
     l=d[i][1:]
+    doc.append(NoEscape(r'\renewcommand*{\arraystretch}{2}'))
     with doc.create(Tabular('|p{1.5cm}|p{7cm}|p{2.5cm}|p{2.5cm}|p{3.5cm}|')) as table:
             table.add_hline()
             table.add_row("S.No","Topics","Start Date","End Date","Remarks")
-            table.add_empty_row()
             for i in l:
                 table.add_hline()
                 h=i.split(" ",1)
                 topic_no=h[0]
                 topic_name=h[1]
                 table.add_row(h[0],h[1],"","","")
-                table.add_empty_row()
             table.add_hline()
-    doc.append(NoEscape(r'\vspace{2mm}'))
-    doc.append('\nEnd Date:______________________')
+    doc.append(NoEscape(r'\vspace{5mm}'))
+    doc.append("\n") 
+    doc.append('End Date:________________________') 
+    doc.append(NoEscape(r'\vspace{5mm}'))
+    doc.append("\n")
     doc.append(NewPage())
 doc.generate_pdf('finalsh3', clean_tex=False)
